290bee1575a50bb489db0fbf7a29ac52375b26d7
Subhan Ahmad <sa9619802@gmail.com>
2024-11-06 15:31:13 +0500

    my-react app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from "./Hello"
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <Hello name="Subhan" class="FSD"/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Hello.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Hello(props) {
+
+
+    return (
+        <div>
+            <h2>Helo, {props.name}</h2>
+            <p>My class is {props.class}</p>
+        </div>
+    )
+}
