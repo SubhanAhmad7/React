a25d8c0a30cb8aa36d80453d0d3da6b7f9bd2544
Subhan Ahmad <sa9619802@gmail.com>
2024-11-18 09:16:47 +0500

    fetch data

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,43 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Hello from "./Hello"
+import { useState } from 'react'
+import Card from './component/Card';
 
 
 
-function App(props) {
+function App() {
+  const [Products, setProducts] = useState([]);
+  const [Loding, setLoding] = useState(false)
 
-const isLogin = "True"
-
+  const getProducts = () => {
+    setLoding(true)
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .finally(() => setLoding(false))
+  }
   return (
-    <div>
-    <h1>Welcome Guys!</h1>
-    <Hello name="Subhan" class="FSD" isLogin={isLogin} />
+    <div className='container'>
+      <button className='btn btn-success' onClick={getProducts}>Get all Products</button>
+      <div className='row'>
+        {
+          (Loding == true) ? (<div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>) : null
+        }
+        {Products.map((product) => {
+          return (
+            <div className="col-md-4" key={product.id}>
+              <Card product={product}/>
+            </div>
+          )
+        })
+        }
+      </div>
     </div>
   )
 }
-
 export default App
--- my-react-app/src/Hello.jsx
+++ my-react-app/src/Hello.jsx
@@ -2,19 +2,23 @@ import Hi from "./Hi"
 import AI from "./Images/AI.webp"
 
 
-export default function Hello(props) {
+// export default function Hello(props) {
 
 
 
 
 
-    return (
-        <div>
-            <h1>{(props.isLogin == "True") ? "Welcome Subhan" : "Welcome Guest"}</h1>
-            <h2>Hello, {props.name}</h2>
-            <p>My class is {props.class}</p>
-            <Hi name={props.name} class={props.class} isLogin={props.isLogin}/>
-            <img src={AI} alt="Not Avariable" width="350px" />
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>{(props.isLogin == "True") ? "Welcome Subhan" : "Welcome Guest"}</h1>
+//             <h2>Hello, {props.name}</h2>
+//             <p>My class is {props.class}</p>
+//             <Hi name={props.name} class={props.class} isLogin={props.isLogin}/>
+//             <img src={AI} alt="Not Avariable" width="350px" /><br /><br />
+//             <button style={{color:"red",padding:"15px 30px"}} onClick={ () =>{props.getCity("Lodhran",props.name) }}> Click Me</button>
+//             <button style={{color:"red",padding:"15px 30px"}} onClick={ () =>{props.setScore(6)}}>Six</button>
+//             <button style={{color:"red",padding:"15px 30px"}} onClick={ () =>{props.setOut(1)}}>Out</button>
+//         </div>
+        
+//     )
+// }
